0c4185904c2cd4dcfb985d2db27fdbf7a3a66e6b
aldaine-AG <aldaineclarke1@gmail.com>
2022-05-14 10:55:05 -0500

    completed the contoller for the order routes

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,21 +1,88 @@
+const dbService = require("../services/db.service");
+const tableName = process.env.DB_NAME + "."+ "orders";
+
+/**
+ * @description Returns an order number based on a random number and the time that the order was made
+ */
+
 class OrderController{
 
 
-    getAllOrders(req, res, next){
+    async getAllOrders(req, res, next){
+        try{
+            const result = await dbService.getAll(tableName);
+            res.json(result);
+        }catch(err){
+            res.json(err);
+        }
 
     }
-    getSingleOrder(req, res, next){
+    async getSingleOrder(req, res, next){
+        let order_num = req.params.id;
+
+        try{
+            const result = await dbService.getOneByFieldName(tableName, "order_num" ,order_num);
+            res.json(result);
+        }catch(err){
+            res.json(err);
+        }
 
     }
-    updateSingleOrder(req, res, next){
+    async updateOrder(req, res, next){
+        let order_num = req.params.id;
+        let data = {
+            status: req.body.status,
+        }
+        try{
+            const result = await dbService.updateByFieldName(tableName, "order_num",order_num, data);
+            res.json(result);
+        }catch(err){
+            res.json(err);
+        }
 
     }
-    createOrder(req, res, next){
+    async createOrder(req, res, next){
+        let data = {
+            order_num: orderNumberGenerator(),
+            customer_id: req.body.cid,
+            status: "pending",
+            date_created: new Date(),
+            total_price: req.body.total_price,
+        }
+        try{
+            const result = await dbService.create(tableName, data);
+            res.json(result);
+        }catch(err){
+            res.json(err);
+        }
 
     }
-    deleteOrder(req, res, next){
-
+    async deleteOrder(req, res, next){
+        let order_num = req.params.id;
+        try{
+            const result = await dbService.deleteRecordByField(tableName, "order_num", order_num);
+            res.json(result);
+        }catch(err){
+            res.json(err);
+        }
     }
 }
+
+function orderNumberGenerator(){
+    // random number between 0 and 99
+    let randNum = Math.floor(Math.random() * 100);
+    let date = new Date()
+    let orderNumString = ""
+
+    let dateArr = [date.getMonth(),date.getDate(),date.getHours(),date.getMinutes()];
+    dateArr.forEach((val)=>{
+        if(val < 10){
+            orderNumString+="0"+ val;
+        }
+        else orderNumString+=val
+    })
+    orderNumString+=randNum;
+    return parseInt(orderNumString,10);
+}  
 
 module.exports = new OrderController();
--- src/routes/order.js
+++ src/routes/order.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const orderController = require("../controllers/orderController");
 
 router.get("/", orderController.getAllOrders);
-router.get("/add", orderController.createOrder);
+router.post("/add", orderController.createOrder);
 router.delete("/:id", orderController.deleteOrder);
 router.get("/:id", orderController.getSingleOrder);
-router.put("/:id", orderController.updateSingleOrder);
+router.put("/:id", orderController.updateOrder);
 
 // TO DO: 
 // Use the library method-overide to alter the request that I receive from the form. from post to put/delete
