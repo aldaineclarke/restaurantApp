9cd253f414a6299b5b54dd8dd2c728f968a3e939
aldaine-AG <aldaineclarke1@gmail.com>
2022-05-16 22:55:59 -0500

    added some routes for the index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const indexController = require('../controllers/indexController');
+const authGuard = require("../middlewares/auth");
 
 router.get("/", indexController.getHomePage);
+
 router.get("/login", indexController.getLoginPage);
+router.get("/about", indexController.getAboutPage);
+router.get("/news", indexController.getNewsPage);
+router.get("/contact", indexController.getContactPage);
+router.get("/cart", indexController.getCartPage);
 
 
 
